8ff983a2779408f6a0da1d46045b516de37b14ec
Dennis Molund <molund1994@gmail.com>
2025-11-02 18:37:51 +0700

    Removed server header for production

--- Web/Program.cs
+++ Web/Program.cs
@@ -12,8 +12,11 @@ using Web.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Removed server header to limit project information to clients.
-builder.WebHost.UseKestrel(options => options.AddServerHeader = false);
+if (builder.Environment.IsProduction())
+{
+    //Removed server header to limit project information to clients.
+    builder.WebHost.UseKestrel(options => options.AddServerHeader = false);
+}
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
