596883826e9812e582336067b09e099b7d14cc4d
dennismolund <43784882+dennismolund@users.noreply.github.com>
2025-11-09 17:50:35 +0700

    Delete WhenTheFireFades.Tests directory
    Deleted old test project

--- WhenTheFireFades.Tests/TestHelpers/TestSessionFeature.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
-
-namespace WhenTheFireFades.Tests.TestHelpers;
-
-public sealed class TestSessionFeature : ISessionFeature
-{
-    public ISession Session { get; set; } = null!;
-}
